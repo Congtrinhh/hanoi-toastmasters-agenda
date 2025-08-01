a8200083d962507fa63863a7eee24779cab4a204
Congtrinhh <trinhquycong@gmail.com>
2025-08-02 02:29:22 +0700

    add public path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: "/hanoi-toastmasters-agenda/",
+	// base: "/hanoi-toastmasters-agenda/",
+	publicPath: process.env.NODE_ENV === "production" ? "/hanoi-toastmasters-agenda/" : "/",
 });
