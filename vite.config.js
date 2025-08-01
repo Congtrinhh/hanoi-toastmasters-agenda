9c5e6aaa619bcaadfdb4f8afdb8aabf01d02942a
Congtrinhh <trinhquycong@gmail.com>
2025-08-02 02:43:08 +0700

    add base again to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
 	plugins: [vue()],
 	base: "/hanoi-toastmasters-agenda/",
-	// publicPath: process.env.NODE_ENV === "production" ? "/hanoi-toastmasters-agenda/" : "/",
 });
